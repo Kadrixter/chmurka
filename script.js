6524a94e8239924f2e137ca63339d4a70393088e
Kadrixter <102877353+Kadrixter@users.noreply.github.com>
2025-01-22 01:38:53 +0100

    file added

---
+++ script.js
@@ -0,0 +1,31 @@
+const cloud = document.querySelector('.cloud');
+
+const randomText = () => {
+  const text = ("!@#$%^*()")
+  return text[Math.floor(Math.random() * text.length)];
+};
+
+const rain = () => {
+  const e = document.createElement('div');
+  e.classList.add('drop');
+  cloud.appendChild(e);
+
+  const left = Math.floor(Math.random() * 300)
+  const size = Math.random() * 1.5;
+  const duration = Math.random() * 1;
+
+  e.innerText = randomText();
+  e.style.left = `${left}px`;
+  e.style.fontSize = `${0.5 + size}em`;
+  e.style.animationDuration = `${1 + duration}s`;
+
+  setTimeout(
+    () => cloud.removeChild(e),
+    1500,
+  );
+};
+
+setInterval(
+  () => rain(),
+  20,
+);
